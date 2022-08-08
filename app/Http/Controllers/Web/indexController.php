5735506799b5dfdaa03f0a55b9ba0d1bff4150e9
Slonexx <21moriarty21@mail.ru>
2022-08-08 17:20:31 +0600

    Check counterparty

--- app/Http/Controllers/Web/indexController.php
+++ app/Http/Controllers/Web/indexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Config\getSettingVendorController;
 use App\Http\Controllers\Config\Lib\cfg;
 use App\Http\Controllers\Config\Lib\VendorApiController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\GuzzleClient\ClientMC;
 use Illuminate\Http\Request;
 
 class indexController extends Controller
@@ -37,12 +38,21 @@ class indexController extends Controller
     }
 
     public function counterparty(Request $request){
+        $cfg = new cfg();
+
         $contextKey = $request->contextKey;
         $vendorAPI = new VendorApiController();
         $employee = $vendorAPI->context($contextKey);
         $accountId = $employee->accountId;
 
-        dd($request);
+        $Setting = new getSettingVendorController($accountId);
+
+        $entity = 'counterparty';
+
+        $getObjectUrl = $cfg->appBaseUrl . "/widgets/get-object.php?accountId=$accountId&entity=$entity&objectId=";
+        $body = new ClientMC($getObjectUrl, $Setting->TokenMoySklad);
+
+        dd( $body->requestGet() );
 
 
 
