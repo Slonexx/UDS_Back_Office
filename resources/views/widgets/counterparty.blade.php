4338505c4d0571a4ac9dd15db30b3e62f4673ece
Slonexx <21moriarty21@mail.ru>
2022-08-09 09:50:11 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -10,9 +10,7 @@
     @endphp
     <br>
     <br>
-    <p><b title="Используя objectId, переданный в сообщении Open, можем получить через JSON API открытую пользователем сущность/документ">
-            Открыт объект
-            <span class="hint">(?)</span>:</b> <span id="object"></span></p>
+    <p><span id="object"></span></p>
 </div>
 @endsection
 
