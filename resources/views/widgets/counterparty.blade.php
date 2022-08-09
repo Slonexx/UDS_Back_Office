b2732f5e32097cb79582c91a6133d2980f301707
Slonexx <21moriarty21@mail.ru>
2022-08-09 15:31:37 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -4,10 +4,10 @@
 @section('counterparty')
 
     <script>
-
+        var globalReceivedMessage;
         window.addEventListener("message", function(event) {
             var receivedMessage = event.data;
-
+            globalReceivedMessage = receivedMessage;
             if (receivedMessage.name === 'Open') {
                 var oReq = new XMLHttpRequest();
                 oReq.addEventListener("load", function() {
@@ -22,7 +22,7 @@
         });
 
         function update(){
-            if (receivedMessage.name === 'Open') {
+            if (globalReceivedMessage.name === 'Open') {
                 var oReq = new XMLHttpRequest();
                 oReq.addEventListener("load", function() {
                     var responseTextPars = JSON.parse(this.responseText);
@@ -30,7 +30,7 @@
                     console.log(" Pars = " + responseTextPars.email)
                 });
 
-                oReq.open("GET", "{{$getObjectUrl}}" + receivedMessage.objectId);
+                oReq.open("GET", "{{$getObjectUrl}}" + globalReceivedMessage.objectId);
                 oReq.send();
             }
         }
