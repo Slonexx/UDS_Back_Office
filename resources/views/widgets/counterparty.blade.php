ff876a5ca6192dcd2aadf4a7a799cb6b803ab06a
Slonexx <21moriarty21@mail.ru>
2022-08-09 17:07:31 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -62,7 +62,7 @@
     }
 
     .myButton {
-        box-shadow: 0px 10px 14px -7px #93e5f3 !important;
+        box-shadow: 0px 0px 5px 0px #93e5f3 !important;
         background-color: #00a6ff !important;
         color: white !important;
         border-radius:50px !important;
