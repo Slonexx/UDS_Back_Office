6a75e532ad51e1f262b49af0af8a92c5bc737821
Slonexx <21moriarty21@mail.ru>
2022-08-09 17:02:45 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -42,7 +42,7 @@
                 <label class="from-label">Клиент </label>
             </div>
             <div class="col-2 p-2">
-                <button type="submit" onclick="update()" class="myButton btn text-orange "> <i class="fa-solid fa-arrow-rotate-right"></i> </button>
+                <button type="submit" onclick="update()" class="myButton btn text-white "> <i class="fa-solid fa-arrow-rotate-right"></i> </button>
             </div>
 
 
@@ -62,7 +62,7 @@
     }
 
     .myButton {
-        background-color: #171717 !important;
+        background-color: orange !important;
         border-radius:50px !important;
         display:inline-block !important;
         cursor:pointer !important;
