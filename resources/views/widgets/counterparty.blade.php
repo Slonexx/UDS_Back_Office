2b0032f180f0e231ef007115a0b80b4cd68d21ca
Slonexx <21moriarty21@mail.ru>
2022-08-09 17:53:50 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -46,7 +46,7 @@
         <div class="row uds-gradient ">
             <div class="mx-2 p-1 col-10 text-white">
                 <img src="https://smartuds.kz/Config/UDS.png" width="30" height="30" class="mx-2" >
-                <label onclick="xRefURL()"> Клиент </label>
+                <label > Клиент </label>
             </div>
             <div class="col-2 p-2">
                 <button type="submit" onclick="update()" class="myButton btn "> <i class="fa-solid fa-arrow-rotate-right"></i> </button>
