9122ae361f487782816be3c38e589b0e4f5813fd
Slonexx <21moriarty21@mail.ru>
2022-08-09 09:59:24 +0600

    Check counterparty

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,6 +14,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        'App\Event\UserCreated' => ['App\Listener']
     ];
 
 
--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -5,12 +5,14 @@
 <div class="content p-1 mt-2 bg-white text-Black rounded">
     @php
 
-
+    dd(app('request'));
 
     @endphp
     <br>
     <br>
-    <p><span id="object"></span></p>
+    <p><b title="Используя objectId, переданный в сообщении Open, можем получить через JSON API открытую пользователем сущность/документ">
+            Открыт объект
+            <span class="hint">(?)</span>:</b> <span id="object"></span></p>
 </div>
 @endsection
 
