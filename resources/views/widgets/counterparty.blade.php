d92abf31b8631f205ee209aca02ddbdd3fc5215c
Slonexx <21moriarty21@mail.ru>
2022-08-09 10:58:04 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -1,81 +1,76 @@
 
 @extends('widgets.index')
 
+<script>
+    var hostWindow = window.parent;
+
+    window.addEventListener("message", function(event) {
+        var receivedMessage = event.data;
+
+        logReceivedMessage(receivedMessage);
+
+        if (receivedMessage.name === 'Open') {
+            var oReq = new XMLHttpRequest();
+            oReq.addEventListener("load", function() {
+                window.document.getElementById("object").innerHTML = this.responseText;
+            });
+            // В демо приложении отсутствует авторизация (между виджетом и бэкендом) - в реальных приложениях не делайте так (должна быть авторизация)!
+            oReq.open("GET", "<?=$getObjectUrl?>" + receivedMessage.objectId);
+            oReq.send();
+
+            window.setTimeout(function() {
+                var sendingMessage = {
+                    name: "OpenFeedback",
+                    correlationId: receivedMessage.messageId
+                };
+                logSendingMessage(sendingMessage);
+                hostWindow.postMessage(sendingMessage, '*');
+
+            }, getOpenFeedbackDelay());
+        }
+    });
+
+    function logReceivedMessage(msg) {
+        logMessage("→ Received", msg)
+    }
+
+    function logSendingMessage(msg) {
+        logMessage("← Sending", msg)
+    }
+
+    function logMessage(prefix, msg) {
+        var messageAsString = JSON.stringify(msg);
+        console.log(prefix + " message: " + messageAsString);
+        addMessage(prefix.toUpperCase() + " " + messageAsString);
+    }
+
+    function addMessage(item) {
+        var messages = window.document.getElementById("messages");
+        messages.innerHTML = item + "<br/>" + messages.innerHTML;
+        messages.title += item + "\n";
+    }
+
+    function getOpenFeedbackDelay() {
+        return window.document.getElementById("openFeedbackDelay").value
+    }
+
+    function toggleBorders(value) {
+        body().className = value ? "borders" : "";
+    }
+
+    function showDimensions() {
+        var dimensions = window.document.getElementById("dimensions");
+        dimensions.innerText = body().offsetWidth + " x " + body().offsetHeight
+    }
+
+    function body() {
+        return window.document.body;
+    }
+</script>
+
 @section('counterparty')
-
-    <script>
-        var hostWindow = window.parent;
-
-        window.addEventListener("message", function(event) {
-            var receivedMessage = event.data;
-
-            logReceivedMessage(receivedMessage);
-
-            if (receivedMessage.name === 'Open') {
-                var oReq = new XMLHttpRequest();
-                oReq.addEventListener("load", function() {
-                    window.document.getElementById("object").innerHTML = this.responseText;
-                });
-                // В демо приложении отсутствует авторизация (между виджетом и бэкендом) - в реальных приложениях не делайте так (должна быть авторизация)!
-                oReq.open("GET", "<?=$getObjectUrl?>" + receivedMessage.objectId);
-                oReq.send();
-
-                window.setTimeout(function() {
-                    var sendingMessage = {
-                        name: "OpenFeedback",
-                        correlationId: receivedMessage.messageId
-                    };
-                    logSendingMessage(sendingMessage);
-                    hostWindow.postMessage(sendingMessage, '*');
-
-                }, getOpenFeedbackDelay());
-            }
-        });
-
-        function logReceivedMessage(msg) {
-            logMessage("→ Received", msg)
-        }
-
-        function logSendingMessage(msg) {
-            logMessage("← Sending", msg)
-        }
-
-        function logMessage(prefix, msg) {
-            var messageAsString = JSON.stringify(msg);
-            console.log(prefix + " message: " + messageAsString);
-            addMessage(prefix.toUpperCase() + " " + messageAsString);
-        }
-
-        function addMessage(item) {
-            var messages = window.document.getElementById("messages");
-            messages.innerHTML = item + "<br/>" + messages.innerHTML;
-            messages.title += item + "\n";
-        }
-
-        function getOpenFeedbackDelay() {
-            return window.document.getElementById("openFeedbackDelay").value
-        }
-
-        function toggleBorders(value) {
-            body().className = value ? "borders" : "";
-        }
-
-        function showDimensions() {
-            var dimensions = window.document.getElementById("dimensions");
-            dimensions.innerText = body().offsetWidth + " x " + body().offsetHeight
-        }
-
-        function body() {
-            return window.document.body;
-        }
-    </script>
-
 <div class="content p-1 mt-2 bg-white text-Black rounded">
-    @php
 
-           {{-- dd(app('request'));--}}
-
-    @endphp
     <br>
     <br>
     <p><b title="Используя objectId, переданный в сообщении Open, можем получить через JSON API открытую пользователем сущность/документ">
