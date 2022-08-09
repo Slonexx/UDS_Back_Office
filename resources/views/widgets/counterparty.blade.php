2553f584671bb79f9dd0c18666fc7c8447bfb339
Slonexx <21moriarty21@mail.ru>
2022-08-09 13:12:14 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -16,6 +16,7 @@
                 });
 
                 oReq.open("GET", "{{$getObjectUrl}}" + receivedMessage.objectId);
+                console.log("objectId = " + receivedMessage.objectId);
                 oReq.send();
             }
         });
@@ -24,6 +25,7 @@
 
     <div class="content p-1 mt-2 bg-white text-Black rounded">
         <h1> Объект =  </h1> <span>{{$getObjectUrl}}</span>
+        <br>
         <p> <span id="object"></span> </p>
 
     </div>
