2beac3c96070f38542ee3f2dad20a9799f1be791
Slonexx <21moriarty21@mail.ru>
2022-08-09 15:54:46 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -36,11 +36,11 @@
     <div class="content p-1 mt-2 bg-white text-Black rounded">
 
         <div class="row">
-            <div class="col-5">
+            <div class="col-10">
                 <img src="https://uds.app/img/fav.png" width="30" height="30" class="mx-4" alt="">
                 <label class="mt-1 from-label">Клиент </label>
             </div>
-            <div class="col-5">
+            <div class="col-2">
                 <button type="submit" onclick="update()" class="btn-new btn text-orange "> <i class="fa-solid fa-arrow-rotate-right"></i> </button>
             </div>
 
