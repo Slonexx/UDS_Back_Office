73006bb77e52aa39da24fae47b0c87e608d92d21
Slonexx <21moriarty21@mail.ru>
2022-08-09 17:21:50 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -42,7 +42,7 @@
         <div class="row uds-gradient ">
             <div class=" p-1col-10 text-white">
                 <img src="https://smartuds.kz/Config/UDS.png" width="30" height="30" class="mx-2" alt="">
-                <label>Клиент <h3 id="displayName" class="mx-5"></h3></label>
+                <label>Клиент <span id="displayName" class="mx-5"></span></label>
 
             </div>
             <div class="col-2 p-2">
