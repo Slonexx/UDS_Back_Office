d8b73e208153bb99c329a83ac2ca999a1f3f9348
Slonexx <21moriarty21@mail.ru>
2022-08-10 10:01:59 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -86,7 +86,7 @@
                 <div class="s-min">Бонусные баллы </div>
             </div>
             <div class="col-4">
-                <span id="points" class="s-min"></span>
+                <span id="points" class="s-min bg-success "></span>
             </div>
 
         </div>
