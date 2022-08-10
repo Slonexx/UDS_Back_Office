aedb4b212dcfefcc37349388e30cbee6dc77972e
Slonexx <21moriarty21@mail.ru>
2022-08-10 09:20:21 +0600

    Check counterparty

--- resources/views/widgets/counterparty.blade.php
+++ resources/views/widgets/counterparty.blade.php
@@ -60,7 +60,7 @@
                 <h6 id="displayName" class=""></h6>
                 </div>
 
-                <div class="col-7 s-min bg-secondary text-white">
+                <div class="col-7 s-min bg-gray text-white">
                     <span> Уровень: </span>
                     <span id="membershipTierName"></span>
                     <span id="membershipTierRate"></span>
@@ -119,6 +119,10 @@
         position: relative !important;
         top: 1px !important;
     }
-
+    .bg-gray{
+        background-color: gray !important;
+        padding: 2px !important;
+        border-radius: 10% !important;
+    }
 
 </style>
