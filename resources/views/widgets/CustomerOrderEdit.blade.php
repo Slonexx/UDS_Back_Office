ca66f842376fb2e54f29af6eaa7c70030f5666e2
Slonexx <21moriarty21@mail.ru>
2022-08-18 18:41:05 +0600

    Object customorder edit widget

--- resources/views/widgets/CustomerOrderEdit.blade.php
+++ resources/views/widgets/CustomerOrderEdit.blade.php
@@ -127,7 +127,7 @@
 
                 </div>
                 <div id="Deleted" class="row" style="display: none;">
-                    <div class="bg-white text-Black rounded" style="display: none">
+                    <div class="bg-white text-Black rounded">
                         <div class="text-center">
                             <div class="p-3 mb-2 bg-danger text-white">
                                 <i class="fa-solid fa-circle-xmark text-danger"></i>
