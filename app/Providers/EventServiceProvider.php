46c8d2c750cd0d277572eda76d2d6c84e1b2ba89
Slonexx <21moriarty21@mail.ru>
2022-08-19 19:30:47 +0600

    loginfo date

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,11 +5,13 @@ namespace App\Providers;
 use App\Models\counterparty_add;
 use App\Models\errorLog;
 use App\Models\order_id;
+use App\Models\order_update;
 use App\Models\webhookClintLog;
 use App\Models\webhookOrderLog;
 use App\Observers\CounterpartyAddModelObserver;
 use App\Observers\ErrorLogModelObserver;
 use App\Observers\orderIDModelObserver;
+use App\Observers\OrderUpdateModelObserver;
 use App\Observers\webhookClientModelObserver;
 use App\Observers\webhookOrderModelObserver;
 use Illuminate\Auth\Events\Registered;
@@ -36,5 +38,6 @@ class EventServiceProvider extends ServiceProvider
         webhookClintLog::observe(webhookClientModelObserver::class);
         webhookOrderLog::observe(webhookOrderModelObserver::class);
         errorLog::observe(ErrorLogModelObserver::class);
+        order_update::observe(OrderUpdateModelObserver::class);
     }
 }
