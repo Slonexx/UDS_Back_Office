feeb398bf6cc82e85a16632042bcdd6d122df096
1ntellekt <pitetvasa@gmail.com>
2022-08-20 21:59:39 +0600

    add attributes log

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -24,7 +24,7 @@ class AttributeController extends Controller
             "accountId" => "required|string"
         ]);
 
-        $this->attributeService->setAllAttributesMs($data["tokenMs"]);
+        $this->attributeService->setAllAttributesMs($data);
 
     }
 
