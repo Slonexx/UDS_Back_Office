99d3921f41963c2e834c2829a3cf52fcede22e8a
Slonexx <21moriarty21@mail.ru>
2022-08-22 16:20:35 +0600

    main

--- app/Http/Controllers/installContoller.php
+++ app/Http/Controllers/installContoller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\BackEnd\BDController;
 use App\Http\Controllers\Config\getSettingVendorController;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
@@ -9,15 +10,20 @@ use Illuminate\Http\Request;
 class installContoller extends Controller
 {
     public function install($accountId){
-        $Setting = new getSettingVendorController($accountId);
+        try {
+            $Setting = new getSettingVendorController($accountId);
 
-        $client = new Client(['base_uri' => 'https://smartuds.kz/api/']);
-        $client->post('attributes',[
-            'headers'=> ['Accept' => 'application/json'],
-            'form_params' => [
-                "tokenMs" => $Setting->TokenMoySklad,
-                "accountId" => $accountId
-            ]
-        ]);
+            $client = new Client(['base_uri' => 'https://smartuds.kz/api/']);
+            $client->post('attributes',[
+                'headers'=> ['Accept' => 'application/json'],
+                'form_params' => [
+                    "tokenMs" => $Setting->TokenMoySklad,
+                    "accountId" => $accountId
+                ]
+            ]);
+        } catch (\Exception $ee) {
+            $bd = new BDController();
+            $bd->errorLog($accountId, $ee->getMessage());
+        }
     }
 }
