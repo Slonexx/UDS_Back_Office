e1eebe0247c7deb63d9b1c11b3b0b42823566198
Slonexx <21moriarty21@mail.ru>
2022-08-30 19:04:57 +0600

    delete mata faka

--- resources/views/main/dump.blade.php
+++ resources/views/main/dump.blade.php
@@ -31,9 +31,8 @@
     }
 
     .gradient{
-        /* background: rgb(145,0,253);
-         background: linear-gradient(34deg, rgba(145,0,253,1) 0%, rgba(232,0,141,1) 100%);*/
-        background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(14,174,87,1) 0%, rgba(12,116,117,1) 90% );
+        background: rgb(145,0,253);
+        background: linear-gradient(34deg, rgba(145,0,253,1) 0%, rgba(232,0,141,1) 100%);
     }
 
 </style>
