56e5034a683cc4634697e5933f89283c97903e6e
Slonexxx <21moriarty21@mail.ru>
2022-09-25 15:23:51 +0600

    edit

--- app/Http/Controllers/Web/SettingController.php
+++ app/Http/Controllers/Web/SettingController.php
@@ -58,10 +58,7 @@ class SettingController extends Controller
             $pool->as('body_store')->withToken($TokenMoySklad)->get($url_store),
             $pool->as('body_productFolder')->withToken($TokenMoySklad)->get($url_productFolder),
         ]);
-        dd($responses['body_productFolder']->object()->rows);
-        if ($ProductFolder == null) {
-            $ProductFolder = ['value' => '0', 'name'=>'Корневая папка' ];
-        };
+
 
 
         return view('web.Setting.index', [
