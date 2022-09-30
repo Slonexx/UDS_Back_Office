21e8acbd91f1fe7993a0f9399c2964d6c1ae0e1f
Slonexxx <21moriarty21@mail.ru>
2022-09-30 13:46:40 +0600

    edit

--- resources/views/widgets/CustomerOrderEdit.blade.php
+++ resources/views/widgets/CustomerOrderEdit.blade.php
@@ -209,7 +209,7 @@
         function onchangeQR(){
             let QRCode = parseInt(document.getElementById("QRCode").value)
             console.log('QRCode = ' + QRCode)
-            if (QRCode > 999999 || QRCode < 99999){
+            if (QRCode > 99999 || QRCode < 999999){
                 alert(QRCode);
             }
         }
