4330e22d83e8e34a990d1d717f5b3d9f62643d8b
Slonexxx <21moriarty21@mail.ru>
2022-10-07 18:26:22 +0600

    edit

--- app/Http/Controllers/BackEnd/ObjectController.php
+++ app/Http/Controllers/BackEnd/ObjectController.php
@@ -274,6 +274,8 @@ class ObjectController extends Controller
             $data['code'] = null;
             $data['phone'] = str_replace("+7", '', $data['user']);
             $data['phone'] = '+7' . str_replace(" ", '', $data['phone']);
+            $data['phone'] = '+8' . str_replace(" ", '', $data['phone']);
+            $data['phone'] = '+9' . str_replace(" ", '', $data['phone']);
         } else {
             $data['code'] = $data['user'];
             $data['phone'] = null;
@@ -298,6 +300,7 @@ class ObjectController extends Controller
                 'skipLoyaltyTotal' => $data['SkipLoyaltyTotal'],
             ],
         ];
+        dd($body);
         try {
             $postBody = $Client->post($url, $body)->purchase;
             return response()->json($postBody);
