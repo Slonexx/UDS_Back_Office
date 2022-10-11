2cc19f280739e3465df289c3e01e67e5be801ec9
Slonexxx <21moriarty21@mail.ru>
2022-10-11 16:07:48 +0600

    edit

--- resources/views/widgets/Salesreturn.blade.php
+++ resources/views/widgets/Salesreturn.blade.php
@@ -203,6 +203,7 @@
         if (v < 0) this.value = 0;
         if (v > return_total) this.value = return_total;
         setPoints = this.value
+        setInnerText_Point_and_Total(setTotal, setPoints);
     });
 
     function only_float(){
