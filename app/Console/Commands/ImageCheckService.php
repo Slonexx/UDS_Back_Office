8fc94b2baff6f02d1c319369cb341398a7bcd725
Slonexxx <21moriarty21@mail.ru>
2023-02-01 12:11:25 +0600

    new

--- app/Console/Commands/ImageCheckService.php
+++ app/Console/Commands/ImageCheckService.php
@@ -28,38 +28,19 @@ class ImageCheckService extends Command
 
     public function handle()
     {
-        $client = new Client();
-        $headers = [
-            'Accept' => 'application/json',
-            'Authorization' => 'Basic NTQ5NzU1ODE5MjkyOk0yWTVNV1U1Tm1VdE5EY3dNaTAwTXpjNExUaGtOall0TkdZM05XUXdNV0ptTnpBMQ=='
-        ];
-        $options = [
-            'multipart' => [
-                [
-                    'name' => 'accountId',
-                    'contents' => '1dd5bd55-d141-11ec-0a80-055600047495'
-                ],
-                [
-                    'name' => 'tokenMs',
-                    'contents' => '320a1fdf9222b8f40c968d0df757a30165a2b9fe'
-                ],
-                [
-                    'name' => 'companyId',
-                    'contents' => '549755819292'
-                ],
-                [
-                    'name' => 'apiKeyUds',
-                    'contents' => 'ZjRkYjgzYjktNjIzNy00OGY1LTg1YmMtMTU1YjRhMWFlZTk0'
-                ],
-                [
-                    'name' => 'folder_id',
-                    'contents' => '0'
-                ],
-                [
-                    'name' => 'store',
-                    'contents' => 'МАГАЗИН'
-                ]
-            ]];
-        return $client->post('https://smartuds.kz/api/productUds', $options);
+
+        $client = new Client(['base_uri' => 'https://smartuds.kz/api/productUds']);
+        $res = $client->post('',[
+            'headers'=> ['Accept' => 'application/json'],
+            'form_params' => [
+                "accountId" => "1dd5bd55-d141-11ec-0a80-055600047495",
+                "tokenMs" => "320a1fdf9222b8f40c968d0df757a30165a2b9fe",
+                "companyId" => "549755819292",
+                "apiKeyUds" => "ZjRkYjgzYjktNjIzNy00OGY1LTg1YmMtMTU1YjRhMWFlZTk0",
+                "folder_id" => "0",
+                "store" => "МАГАЗИН",
+            ]
+        ]);
+        dd($res);
     }
 }
