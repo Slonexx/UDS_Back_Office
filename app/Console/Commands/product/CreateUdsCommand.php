2cd1dae8e20b0cbb2893b27e66a07f66864d765a
Slonexxx <21moriarty21@mail.ru>
2023-02-06 19:40:43 +0600

    error

--- app/Console/Commands/product/CreateUdsCommand.php
+++ app/Console/Commands/product/CreateUdsCommand.php
@@ -6,12 +6,16 @@ use App\Components\MsClient;
 use App\Components\UdsClient;
 use App\Http\Controllers\Config\getSettingVendorController;
 use App\Http\Controllers\ProductController;
+use App\Services\product\ProductCreateUdsService;
 use App\Services\Settings\SettingsService;
 use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Promise\Each;
 use GuzzleHttp\Promise\EachPromise;
 use Illuminate\Console\Command;
-
-
+use Illuminate\Http\Client\Pool;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Generator;
 
 class CreateUdsCommand extends Command
 {
@@ -33,7 +37,48 @@ class CreateUdsCommand extends Command
     public function handle()
     {
         $allSettings = $this->settingsService->getSettings();
-        $promises  = function ($allSettings) {
+        $responses = Http::pool(function (Pool $pool) use ($allSettings) {
+            return [
+                Each::ofLimit(
+                    (function () use ($pool, $allSettings): Generator {
+                        foreach ($allSettings as $settings) {
+                            try {
+                                $ClientCheckMC = new MsClient($settings->TokenMoySklad);
+                                $body = $ClientCheckMC->get('https://online.moysklad.ru/api/remap/1.2/entity/employee');
+
+                                $ClientCheckUDS = new UdsClient($settings->companyId, $settings->TokenUDS);
+                                $body = $ClientCheckUDS->get('https://api.uds.app/partner/v2/settings');
+                            } catch (\Throwable $e) { continue; }
+
+                            if ($settings->TokenUDS == null || $settings->companyId == null || $settings->UpdateProduct == "1"){ continue; }
+                            if ( $settings->ProductFolder == null) $folder_id = '0'; else $folder_id = $settings->ProductFolder;
+
+                            $data = [
+                                "tokenMs" => $settings->TokenMoySklad,
+                                "companyId" => $settings->companyId,
+                                "apiKeyUds" => $settings->TokenUDS,
+                                "folder_id" => $folder_id,
+                                "store" => $settings->Store,
+                                "accountId" => $settings->accountId,
+                            ];
+                            yield app(ProductCreateUdsService::class)->insertToUds($data);
+
+                        }
+                    })(),
+                    count($allSettings), // this is the concurrency
+                    function (Response $response) {
+                        dump($response->effectiveUri()->getPath());
+                    }
+                )
+            ];
+        });
+
+        foreach($responses as $response) {
+            dump((string)$response->effectiveUri());
+        }
+
+
+        /* $promises  = function ($allSettings) {
             foreach ($allSettings as $settings){
                 try {
                     $ClientCheckMC = new MsClient($settings->TokenMoySklad);
@@ -77,7 +122,7 @@ class CreateUdsCommand extends Command
         $all->promise()->wait();
         //$responses  = Pool::batch($client, $requests($allSettings), ['concurrency' => count($allSettings)]);
 
-
+*/
 
         /*
         $allSettings = $this->settingsService->getSettings();
