7bba54c74c06b663399f795283a444b61da3b310
Slonexxx <21moriarty21@mail.ru>
2023-08-22 21:38:58 +0600

    new update

--- app/Console/Commands/CronCommandAgentCreate.php
+++ app/Console/Commands/CronCommandAgentCreate.php
@@ -6,67 +6,78 @@ use App\Components\MsClient;
 use App\Components\UdsClient;
 use App\Http\Controllers\BD\getMainSettingBD;
 use App\Models\newAgentModel;
-use App\Models\newProductModel;
 use App\Services\newAgentService\createAgentForMS;
-use App\Services\newProductService\createProductForMS;
-use App\Services\newProductService\createProductForUDS;
-use GuzzleHttp\Exception\BadResponseException;
 use Illuminate\Support\Facades\Cache;
+use GuzzleHttp\Exception\BadResponseException;
 use Illuminate\Console\Command;
 
 class CronCommandAgentCreate extends Command
 {
-
     protected $signature = 'cronCommand:AgentCreate';
-
     protected $description = ' ';
+
     public function __construct()
     {
         parent::__construct();
     }
+
     public function handle(): void
     {
-        $mutex = Cache::lock('process_NewAgent', 9000);
+        $mutex = Cache::lock('process_NewAgents', 3600);
 
-        if ( $mutex->get() ) {
-            $allSettings = newAgentModel::all();
+        if (!$mutex->get()) {
+            $this->info('Previous task is still running. Skipping the current run.');
+            return;
+        }
 
-            foreach ($allSettings as $item) {
-                $mainSetting = new getMainSettingBD($item->getAttributes()['accountId']);
-                try {
-                    $ClientCheckMC = new MsClient($mainSetting->tokenMs);
-                    $ClientCheckMC->get('https://online.moysklad.ru/api/remap/1.2/entity/employee');
-                    $ClientCheckUDS = new UdsClient($mainSetting->companyId, $mainSetting->TokenUDS);
-                    $ClientCheckUDS->get('https://api.uds.app/partner/v2/settings');
-                } catch (BadResponseException) { continue; }
-                if ($item->getAttributes()['unloading'] == '0' or $item->getAttributes()['unloading'] == null) continue;
+        $allSettings = newAgentModel::all();
 
-                $data = [
-                    'accountId' => $item->getAttributes()['accountId'],
-
-                    'unloading' => $item->getAttributes()['unloading'],
-                    'examination' => $item->getAttributes()['examination'],
-                    'email' => $item->getAttributes()['email'],
-                    'gender' => $item->getAttributes()['gender'],
-                    'birthDate' => $item->getAttributes()['birthDate'],
-
-                    'url' => $item->getAttributes()['url'],
-                    'offset' => $item->getAttributes()['offset'],
-                ];
-                if ($item->getAttributes()['unloading'] ==  null or $item->getAttributes()['unloading'] ==  '0') continue;
-
-
-                try {
-                    $create = new createAgentForMS($data, $ClientCheckMC, $ClientCheckUDS);
-                    $create->initialization();
-                } catch (BadResponseException) {
-                    continue;
-                }
+        foreach ($allSettings as $item) {
+            if (!$this->shouldProcessItem($item)) {
+                continue;
             }
 
-            $mutex->release();
-        } else {
-            $this->info('Previous task is still running. Skipping the current run.');
+            $mainSetting = new getMainSettingBD($item->accountId);
+            $this->processItem($item, $mainSetting);
+        }
+
+        $mutex->release();
+    }
+
+    private function shouldProcessItem($item): bool
+    {
+        $unloadingValue = $item->getAttributes()['unloading'];
+        return !empty($unloadingValue) && $unloadingValue !== '0';
+    }
+
+    private function processItem($item, $mainSetting): void
+    {
+        try {
+            $clientCheckMC = new MsClient($mainSetting->tokenMs);
+            $clientCheckMC->get('https://online.moysklad.ru/api/remap/1.2/entity/employee');
+
+            $clientCheckUDS = new UdsClient($mainSetting->companyId, $mainSetting->TokenUDS);
+            $clientCheckUDS->get('https://api.uds.app/partner/v2/settings');
+        } catch (BadResponseException) {
+            return;
+        }
+
+        $data = [
+            'accountId' => $item->accountId,
+            'unloading' => $item->unloading,
+            'examination' => $item->examination,
+            'email' => $item->email,
+            'gender' => $item->gender,
+            'birthDate' => $item->birthDate,
+            'url' => $item->url,
+            'offset' => $item->offset,
+        ];
+
+        try {
+            $create = new createAgentForMS($data, $clientCheckMC, $clientCheckUDS);
+            $create->initialization();
+        } catch (BadResponseException) {
+            return;
         }
     }
 }
