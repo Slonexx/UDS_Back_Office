23dc732d8e77ca418a4a3024812ba109787f91ed
Slonexxx <21moriarty21@mail.ru>
2023-08-22 23:53:27 +0600

    new update

--- resources/views/widget/object.blade.php
+++ resources/views/widget/object.blade.php
@@ -251,17 +251,17 @@
         let operationsAccrue
         let operationsCancellation
 
-        let receivedMessage = {
+        /*let receivedMessage = {
             "name": "Open",
             "extensionPoint": "document.customerorder.edit",
             "objectId": "5f3023e9-05b3-11ee-0a80-06f20001197a",
             "messageId": 1,
             "displayMode": "expanded"
-        }
+        }*/
 
 
         window.addEventListener("message", function (event) {
-            //let receivedMessage = event.data
+            let receivedMessage = event.data
 
             console.log(receivedMessage);
             GlobalobjectId = receivedMessage.objectId;
@@ -272,7 +272,7 @@
                 console.log('initial request settings  ↓ ')
                 console.log(settings)
 
-                receivedMessage = [];
+                //receivedMessage = [];
 
                 $.ajax(settings).done(function (response) {
                     console.log('initial request response  ↓ ')
