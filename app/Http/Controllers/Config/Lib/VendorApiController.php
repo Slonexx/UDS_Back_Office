62130c16f84694a8a942ca047f3a3745580d102c
Slonexxx <21moriarty21@mail.ru>
2023-09-15 10:27:46 +0600

    new update

--- app/Http/Controllers/Config/Lib/VendorApiController.php
+++ app/Http/Controllers/Config/Lib/VendorApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Config\Lib;
 use App\Http\Controllers\Controller;
 
 use \Firebase\JWT\JWT;
-use Illuminate\Support\Facades\Config;
 
 require_once 'jwt.lib.php';
 
@@ -23,7 +22,7 @@ class VendorApiController extends Controller
     }
 
     private function request(string $method, $path, $body = null) {
-        $url = Config::get("Global.moyskladVendorApiEndpointUrl") . $path;
+        $url = (new cfg())->moyskladVendorApiEndpointUrl . $path;
         return makeHttpRequest( $method, $url, buildJWT(), $body);
     }
 
@@ -36,7 +35,6 @@ function makeHttpRequest(string $method, string $url, string $bearerToken, $body
                 'header'  => array(
                     'Authorization: Bearer ' . $bearerToken,
                     "Content-type: application/json",
-                    'Accept-Encoding: ing'
                 ),
                 'content' => $body
             )
@@ -44,10 +42,7 @@ function makeHttpRequest(string $method, string $url, string $bearerToken, $body
         : array('http' =>
             array(
                 'method'  => $method,
-                'header'  => array(
-                    'Authorization: Bearer ' . $bearerToken,
-                    'Accept-Encoding: ing' // Добавленный заголовок Accept-Encoding
-                )
+                'header'  => 'Authorization: Bearer ' . $bearerToken
             )
         );
     $context = stream_context_create($opts);
--- config/Global.php
+++ config/Global.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'moyskladVendorApiEndpointUrl' =>  'https://apps-api.moysklad.ru/api/vendor/1.0',
+    'moyskladVendorApiEndpointUrl' =>  'https://online.moysklad.ru/api/vendor/1.0',
     'moyskladJsonApiEndpointUrl' =>  'https://online.moysklad.ru/api/remap/1.2',
 ];
