6d2306a96e2a9bc67094efe7563e8d7f64abf8cb
Slonexxx <21moriarty21@mail.ru>
2023-09-21 15:39:18 +0600

    new update api ms

--- app/Services/counterparty/widgetCounterparty.php
+++ app/Services/counterparty/widgetCounterparty.php
@@ -17,35 +17,55 @@ class widgetCounterparty
 
         try {
             $MSCounterparty = $ClientMS->get('https://api.moysklad.ru/api/remap/1.2/entity/counterparty/' . $objectId);
-        } catch (BadResponseException $e) {
+        } catch (BadResponseException) {
             return response()->json(['Bool' => false]);
         }
-        if (is_numeric($MSCounterparty->externalCode) && ctype_digit($MSCounterparty->externalCode) && $MSCounterparty->externalCode > 10000) {
 
+        if (is_numeric($MSCounterparty->externalCode) && ctype_digit($MSCounterparty->externalCode) && $MSCounterparty->externalCode > 10000) {
             try {
                 return response()->json([
                     'Bool' => true,
                     'customers' => $ClientUDS->get('https://api.uds.app/partner/v2/customers/' . $MSCounterparty->externalCode)
                 ]);
             } catch (BadResponseException) {
-                return response()->json(['Bool' => false]);
+
+                if (property_exists($MSCounterparty, 'phone')) {
+                    $phone = "+7" . mb_substr(str_replace('+7', '', str_replace(" ", '', $MSCounterparty->phone)), -10);
+
+                    try {
+                        return response()->json([
+                            'Bool' => true,
+                            'customers' => $ClientMS->get('https://api.uds.app/partner/v2/customers/find?phone=' . $phone)->user
+                        ]);
+
+                    } catch (BadResponseException $e) {
+                        return response()->json(['Bool' => false]);
+                    }
+
+                } else {
+                    return response()->json(['Bool' => false]);
+                }
+
             }
 
         } else {
 
             if (property_exists($MSCounterparty, 'phone')) {
-                $phone = "+7" . mb_substr(str_replace('+7', '', str_replace(" ", '', $MSCounterparty->phone)), -10);
-
+                $phone = "%2b7" . mb_substr(str_replace('+7', '', str_replace(" ", '', $MSCounterparty->phone)), -10);
                 try {
-                    return $ClientMS->get('https://api.uds.app/partner/v2/customers/find?phone=' . $phone)->user;
-                } catch (BadResponseException $e) {
+                    return response()->json([
+                        'Bool' => true,
+                        'customers' => $ClientMS->get('https://api.uds.app/partner/v2/customers/find?phone=' . $phone)->user
+                    ]);
+
+                } catch (BadResponseException) {
                     return response()->json(['Bool' => false]);
                 }
 
             } else {
                 return response()->json(['Bool' => false]);
             }
-        }return response()->json(['Bool' => false]);
+        }
     }
 
 }
