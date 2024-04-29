b81771e57951ad8358574f794a97355edc0e71f9
Slonexxx <21moriarty21@mail.ru>
2024-04-29 13:36:45 +0600

    fix

--- app/Console/Commands/product/CronCommandProductCreate.php
+++ app/Console/Commands/product/CronCommandProductCreate.php
@@ -25,7 +25,7 @@ class CronCommandProductCreate extends Command
 
     public function handle(): void
     {
-        $mutex = Cache::lock('process_NewProduct', 9000);
+        $mutex = Cache::lock('process_NewProduct_', 9000);
         if ($mutex->get()) {
         //if (true) {
             $allSettings = newProductModel::all();
@@ -49,15 +49,17 @@ class CronCommandProductCreate extends Command
                 if ($item['unloading'] == null) continue;
                 try {
 
-                    dispatch(function () use ($data, $sql) {
-                        $sql->countRound += 1;
-                        $sql->save();
+                    $sql->countRound += 1;
+                    $sql->save();
 
-                        if ($data['loading']) $create = new createProductForMS($data);
-                        else $create = new createProductForUDS($data);
+                    if ($data['loading']) $create = new createProductForMS($data);
+                    else $create = new createProductForUDS($data);
 
-                        if ($data['countRound'] < 3) $create->initialization();
-                    })->onQueue('default');
+                    if ($data['countRound'] < 3) $create->initialization();
+
+                  /*  dispatch(function () use ($data, $sql) {
+
+                    })->onQueue('default');*/
 
                     $this->info($data['accountId'].': успешно.');
                 } finally {
